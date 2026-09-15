5a0df7d42e5b8073d7676b097c921b37f1cbd132
joakimran5 <joakimran5@gmail.com>
2026-09-15 12:33:25 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("Hello from Vanilla JavaScript!");
+
+// Simple function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Use the function and print the result
+const res = add(5, 7);
+console.log("The sum of 5 and 7 is:", res);
